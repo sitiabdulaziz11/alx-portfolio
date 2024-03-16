79c0fd909a6ba2ef4576b4768bdff9b113b61c0b
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-03-16 10:51:29 +0300

    updated

--- portfolio/models.py
+++ portfolio/models.py
@@ -8,7 +8,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    # password = db.Column(db.String(60), nullable=False) password as plain text.
+    password = db.Column(db.String(60), nullable=False)
     
     
     def __repr__(self):
