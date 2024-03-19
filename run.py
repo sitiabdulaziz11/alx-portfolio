8bb08cc4597f104a2bd0fee83169efa01a0a106b
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-03-19 15:52:41 +0300

    debug

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ Main backe end module or routes script.
 from portfolio import app
 
 
-if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+#if __name__ == '__main__':
+    #app.run(debug=False,host='0.0.0.0')
